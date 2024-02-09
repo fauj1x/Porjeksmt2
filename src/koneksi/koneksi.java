9c7e8d12e29813fa8b04603678a1ad35b4d855e9
fauji2317 <114175922+fauji2317@users.noreply.github.com>
2024-02-09 23:27:58 +0700

    test kedua

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package koneksi;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class koneksi {
+            public static Connection koneksi;
+                        public static Connection GetConnection(){
+                                    
+                                    try {
+                                        String url = "jdbc:mysql://localhost:3306/rohingya";
+                                        String user = "root";
+                                        String password = "";
+
+
+                                        koneksi= DriverManager.getConnection(url, user, password);
+                                    } catch (SQLException e) {
+                                        JOptionPane.showMessageDialog(null, "Koneksi gagal: " + e.getMessage());
+                                    }
+                                    return koneksi;
+
+
+                        }
+            }
+
+    
+
